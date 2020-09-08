66e82313fbd1bcd88acef5b35ce38d142b1388b9
Theo <theojepsen@g-m-a-i-l-dotcom>
2020-09-08 21:18:09 +0200

    load trace file manualy and include lnic

--- tools/tool_serial_classifier.cpp
+++ tools/tool_serial_classifier.cpp
@@ -35,10 +35,21 @@
 #include <serial_nuevomatch.h>
 #include <nuevomatch_config.h>
 #if 1
-#include "nuevomatch_64_classifier.h"
+//#include "nuevomatch_64_classifier.h"
+#include "nuevomatch_64_classifier_100rules.h"
 #else
 char nuevomatch_64_classifier[] = {0};
 #endif
+
+#include "lnic.h"
+
+extern "C" {
+    extern int _end;
+    char* data_end = (char*)&_end + 16384*4;
+    extern void sbrk_init(long int* init_addr);
+    extern void __libc_init_array();
+    extern void __libc_fini_array();
+}
 
 /**
  * @brief Main entry point
@@ -83,6 +94,7 @@ int main(int argc, char** argv) {
 
   // Read the textual trace file
 
+#if 0
   messagef("Reading trace file...");
   const char* trace_filename = "trace";
   uint32_t num_of_packets;
@@ -91,6 +103,20 @@ int main(int argc, char** argv) {
   if (!trace_packets) {
     throw error("error while reading trace file");
   }
+#else
+  uint32_t num_of_packets = 10;
+  trace_packet trace_packets[10];
+  trace_packets[0].header = {1824690037, 2405068276, 6394,  15121, 6, 0}; trace_packets[0].match_priority = 23682;
+  trace_packets[1].header = {2735466734, 2409248971, 24383, 33147, 6, 0}; trace_packets[1].match_priority = 80749;
+  trace_packets[2].header = {3014326451, 224173574,  14968, 1715,  6, 0}; trace_packets[2].match_priority = 61796;
+  trace_packets[3].header = {4000926507, 1669083352, 46568, 135,   6, 0}; trace_packets[3].match_priority = 37982;
+  trace_packets[4].header = {3490938347, 1673468417, 19902, 1986,  6, 0}; trace_packets[4].match_priority = 76049;
+  trace_packets[5].header = {1711012005, 1662762722, 33779, 23972, 6, 0}; trace_packets[5].match_priority = 69447;
+  trace_packets[6].header = {3519978292, 1673501826, 53790, 1521,  6, 0}; trace_packets[6].match_priority = 4905;
+  trace_packets[7].header = {3358996848, 1677639773, 45651, 1310,  6, 0}; trace_packets[7].match_priority = 60033;
+  trace_packets[8].header = {962377704,  1664972678, 23949, 1521,  6, 0}; trace_packets[8].match_priority = 14597;
+  trace_packets[9].header = {500138621,  3396495291, 35485, 5631,  6, 0}; trace_packets[9].match_priority = 49831;
+#endif
   messagef("Total %u packets in trace", num_of_packets);
 
   // Limit the number of packets
@@ -98,10 +124,13 @@ int main(int argc, char** argv) {
   uint32_t end_packet   = -1;
   if (end_packet > num_of_packets) end_packet = num_of_packets;
 
+  uint64_t start_cycles, delta_cycles;
+
   // Warm cache
   uint32_t warm_repetitions = 5;
   for (uint32_t r=0; r<warm_repetitions; ++r) {
     messagef("Iteration %u...", r);
+    start_cycles = rdcycle();
     for (uint32_t i=start_packet; i<end_packet; ++i) {
       classifier_output_t out = classifier->classify(trace_packets[i].get());
       if ((uint32_t)out.action != trace_packets[i].match_priority) {
@@ -110,6 +139,8 @@ int main(int argc, char** argv) {
       }
     }
     classifier->reset_counters();
+    delta_cycles = rdcycle() - start_cycles;
+    printf("Latency: %ld cycles total, %ld cycles/packet\n", delta_cycles, delta_cycles/num_of_packets);
   }
 
  	// Perform the experiment, repeat X times
@@ -121,7 +152,7 @@ int main(int argc, char** argv) {
 
     // Reset counters
     classifier->reset_counters();
-
+    start_cycles = rdcycle();
     classifier->start_performance_measurement();
     // Run the lookup
     for (uint32_t i=start_packet; i<end_packet; ++i) {
@@ -131,6 +162,8 @@ int main(int argc, char** argv) {
 							i, out.action, trace_packets[i].match_priority);
       }
     }
+    delta_cycles = rdcycle() - start_cycles;
+    printf("Latency: %ld cycles total, %ld cycles/packet\n", delta_cycles, delta_cycles/num_of_packets);
 
     classifier->stop_performance_measurement();
 
@@ -142,4 +175,19 @@ int main(int argc, char** argv) {
  	}
 
  	delete classifier;
+}
+
+extern "C" {
+bool is_single_core() { return false; }
+int core_main(int argc, char** argv, int cid, int nc) {
+  (void)nc;
+  if (cid > 0) return EXIT_SUCCESS;
+
+  // Setup the C++ libraries
+  sbrk_init((long int*)data_end);
+  atexit(__libc_fini_array);
+  __libc_init_array();
+
+  return main(argc, argv);
+}
 }
